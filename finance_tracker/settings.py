7df6818fb267382753ce8d04a25dad40da0824a2
Chandana T J <chandanatj030@gmail.com>
2026-09-25 16:38:34 +0530

    Add production HTTPS configuration

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = []
 
